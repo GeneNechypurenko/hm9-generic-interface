33b1919f280c29d6e139da058e17fd82b595bdbf
Eugene Nechypurenko <e.nechypurenko@gmail.com>
2023-10-03 14:36:23 +0300

    complete

---
+++ hm9/MyArray.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace hm9
+{
+    internal class MyArray : ICalc, IOutput
+    {
+        public int Size { get; set; }
+        private int[] _arr;
+        public MyArray(int size)
+        {
+            _arr = new int[size];
+            Size = size;
+        }
+        public int this[int i]
+        {
+            get { return _arr[i]; }
+            set { _arr[i] = value; }
+        }
+        public static MyArray operator +(MyArray a, MyArray b)
+        {
+            MyArray result = new MyArray(a.Size);
+
+            for (int i = 0; i < a.Size; ++i)
+            {
+                result[i] = (dynamic)a[i] + b[i];
+            }
+
+            return result;
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < Size; ++i)
+            {
+                sb.Append($"index[{i}]: {this[i]}\n");
+            }
+
+            return sb.ToString();
+        }
+        public void ReSize(int newSize)
+        {
+            int[] newArr = new int[newSize];
+            Size = newSize;
+            _arr = newArr;
+        }
+        public void Init()
+        {
+            for (int i = 0; i < _arr.Length; ++i)
+            {
+                _arr[i] = i;
+            }
+        }
+        public void InitWithValue(int value)
+        {
+            for (int i = 0; i < _arr.Length; ++i)
+            {
+                _arr[i] = value;
+            }
+        }
+        public void InitWithRand(int min, int max)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < _arr.Length; ++i)
+            {
+                _arr[i] = rand.Next(min, max);
+            }
+        }
+        public int Less(int valueToCompare)
+        {
+            int count = 0;
+
+            foreach (int item in _arr)
+            {
+                if (item < valueToCompare)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        public int Greater(int valueToCompare)
+        {
+            int count = 0;
+
+            foreach (int item in _arr)
+            {
+                if (item > valueToCompare)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public void ShowEven()
+        {
+            foreach (int item in _arr)
+            {
+                if (item % 2 == 0)
+                {
+                    Console.WriteLine($"Even = {item}");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void ShowOdd()
+        {
+            foreach (int item in _arr)
+            {
+                if (item % 2 == 1)
+                {
+                    Console.WriteLine($"Odd = {item}");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- hm9/Program.cs
+++ hm9/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using hm9;
+
+MyArray array = new MyArray(2);
+Random rnd = new Random();
+
+array.ReSize(10);
+
+array.InitWithRand(9, 99);
+
+Console.WriteLine(array);
+
+int valueToCompare = rnd.Next(9, 99);
+
+Console.WriteLine($"LESS then {valueToCompare} = {array.Less(valueToCompare)}\n");
+Console.WriteLine($"GREATER then {valueToCompare} = {array.Greater(valueToCompare)}\n");
+
+array.ShowEven();
+array.ShowOdd();
